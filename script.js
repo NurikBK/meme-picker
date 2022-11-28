672b2f4723e06c98772f824d7729d6bb4b2ea5f6
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-28 17:55:36 +0600

    add checkbox eventListener

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 import { catsData } from '/data.js';
 const emotionRadios = document.getElementById('emotion-radios');
+const getImageBtn = document.getElementById('get-image-btn');
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 renderEmotionsRadios();
 
 emotionRadios.addEventListener('change', highlightCheckedOption);
+getImageBtn.addEventListener('click', getMatchingCatsArray);
 
 function highlightCheckedOption(e) {
+  const radios = document.querySelectorAll('.radio');
+  radios.forEach((radio) => radio.classList.remove('highlight'));
   let id = e.target.id;
-  document.getElementById(id).parentElement.classList.toggle('highlight');
+  document.getElementById(id).parentElement.classList.add('highlight');
 }
 
 function getEmotionsArray(cats) {
@@ -21,6 +26,16 @@ function getEmotionsArray(cats) {
   }
 
   return emotionsArr;
+}
+
+function getMatchingCatsArray() {
+  const isGif = gifsOnlyOption.checked
+
+
+  const selectedEmotion = document.querySelector('input[type="radio"]:checked');
+  if (selectedEmotion) {
+    console.log(selectedEmotion.value); 
+  }
 }
 
 function renderEmotionsRadios() {
